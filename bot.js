a73c3fa7b5efa213f0d94a3ddcc3b34873255f8e
LuxAestatis <luxaestatis3403@gmail.com>
2016-12-20 23:10:58 -0500

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,20 @@
-var Discord = require('discord.io');
-var bot = new Discord.Client({
-    token: "MjYwODgzMDMwMTY2NjAxNzI5.Czs1xg.0k9C1X3WOPWHahzFOAfqO1KefcU",
-    autorun: true
-});
+// var Discord = require('discord.io');
+// var bot = new Discord.Client({
+//     token: "MjYwODgzMDMwMTY2NjAxNzI5.Czs1xg.0k9C1X3WOPWHahzFOAfqO1KefcU",
+//     autorun: true
+// });
 
-bot.on('ready', function() {
-    console.log(bot.username + " - (" + bot.id + ")");
-});
+// bot.on('ready', function() {
+//     console.log(bot.username + " - (" + bot.id + ")");
+// });
 
-bot.on('message', function(user, userID, channelID, message, event) {
-    if (message === "ping") {
-        bot.sendMessage({
-            to: channelID,
-            message: "pong"
-        });
-    }
-});
+// bot.on('message', function(user, userID, channelID, message, event) {
+//     if (message === "ping") {
+//         bot.sendMessage({
+//             to: channelID,
+//             message: "pong"
+//         });
+//     }
+// });
+
+console.log("Hello world!");
